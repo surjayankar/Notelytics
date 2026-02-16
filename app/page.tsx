d6b688a938f6ba4b4da26cbd5afda8e3d81a7fc1
Surjayan Kar <surjayan.smartboy@gmail.com>
2026-02-16 20:19:52 +0530

    Implemented Features section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 
 import Image from "next/image";
 import HeroSection from "./components/landing/HeroSection";
+import FeatureSection from "./components/landing/FeatureSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
       <HeroSection/>
+      <FeatureSection/>
     </div>
   );
 }
